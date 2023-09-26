4dc567480fdb983229c6ff644a758a04b5b6e09a
Malik_Kashif <malikmuhammadkashif414@gmail.com>
2023-09-26 22:30:25 +0500

    Bar added

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,98 +1,15 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Kashif from "../Assets/kashifsaeed.png";
-
-import '../Style/MainPage.css';
-
-import { makeStyles } from '@material-ui/core/styles';
+import Section1 from '../Component/Section1'
+import SnackBar from '../Component/SnackBar'
 
 
-
-
-
-const useStyles = makeStyles((theme) => ({
-    container: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundImage: 'linear-gradient(45deg, #874da2 0%, #c43a30 100%)', // Adjust the background color as needed
-      borderRadius: theme.spacing(1),
-      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Add a shadow effect
-      height: '100vh',
-      padding: '50px'
-    },
-    textContainer: {
-      flex: 1,
-    },
-    text: {
-      marginBottom: theme.spacing(2),
-      color: '#65FDF0',
-      fontSize: '1.1rem'
-
-    },
-    welcome: {
-        marginBottom: theme.spacing(2),
-        fontSize: '3.44rem',
-        color: '#65FDF0',
-        fontWeight: 900
-        
-      },
-    floatingImage: {
-      position: 'relative',
-      animation: '$float 4s ease-in-out infinite',
-      backgroundColor: 'transparent'
-    },
-    '@keyframes float': {
-      '0%, 100%': {
-        transform: 'translateY(0)',
-      },
-      '50%': {
-        transform: 'translateY(-20px)', // Adjust this value to control the float height
-      },
-    },
-    image: {
-      width: '100%',
-      height: 'auto',
-      maskImage: 'linear-gradient(45deg, #874da2 0%, #c43a30 100%)'
-    },
-  }));
 
 export default function MainPage() {
-    const classes = useStyles();
-
     return (
         <>
-        <div className='Main-Page'>
-        <div className={classes.container}>
-            <Grid container spacing={2}>
-                <Grid xsm={12} sm={11} md={6}>
-                <div className={classes.textContainer}>
-                    <h3 className={classes.welcome}>Welcome to my <span style={{
-                        background: `-webkit-linear-gradient(to top, #4481eb, #04befe)`,
-                        WebkitBackgroundClip: 'text',
-                        color: '#0504aa',
-                        display: 'inline-block',
-                        fontWeight: 'bold',
-                        
-                    }}>services website</span></h3>
-                    <p className={classes.text}>
-                    Describe your AI/ML services or any other information you want to display on the left side.
-                    </p>
-                </div>
-                </Grid>
-                <Grid xs={12} sm={11} md={4}>
-                    <div className={classes.floatingImage}>
-                        <img
-                        src={Kashif} // Replace with the URL of your image
-                        alt="Floating Image"
-                        className={`${classes.image} enlarged-image`}
-                        />
-                    </div>
-                </Grid>
-        </Grid>
-        </div>
+        <SnackBar />
+        <Section1 />
 
-        </div>
         </>
     )
 }
