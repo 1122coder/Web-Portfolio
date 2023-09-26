69ca4f7ddb21d1b52aa9250af4b399357430e3cc
Malik_Kashif <malikmuhammadkashif414@gmail.com>
2023-09-26 21:12:06 +0500

    Component page created

---
+++ src/Pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from './MainPage';
+
+export default function Main() {
+    return (
+        <>
+        <div className='Main-Page'>
+            <MainPage />
+        </div>
+        </>
+    )
+}
